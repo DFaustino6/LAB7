8cea42d38f44776f4fb77edb8b93dcdd156de2ce
DFaustino6 <a61171@ualg.pt>
2019-11-04 22:28:31 +0000

    Update updateblog_action.php

--- updateblog_action.php
+++ updateblog_action.php
@@ -1,20 +1,22 @@
 <?php
 include 'db.php';
 // ligação à base de dados
+session_start();
 $db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
 if($db) {
   // criar query numa string
   $PostId=$_GET['PostId'];
-  $query  = "INSERT INTO microposts(content,updated_at) VALUES ('$',NOW()) WHERE id='$PostId'";
- 
-  // executar a query
-  if(!($result = @ mysql_query($query,$db )))
-  	showerror();
-
-$nrows  = mysql_num_rows($result);
-   for($i=0; $i<$nrows; $i++)
-     $tuple[$i] = mysql_fetch_array($result,MYSQL_ASSOC);
-
-  mysql_close($db);
+  if(isset($_GET['PostId'])){
+ 	 $query  = "SELECT * FROM microposts WHERE id='$PostId'";
+ 	 if(!($result = @ mysql_query($query,$db)))
+   			showerror();
+   	 $nrows  = mysql_num_rows($result);
+   	 if(isset($_SESSION['username']) && $nrows>0){
+   	 	$content=$_REQUEST['postContent'];
+  		$query  = "UPDATE microposts SET content=$content WHERE id='$PostId'";
+  		$result= @ mysql_query($query,$db);
+  }
+  }
 } 
+mysql_close($db);
 ?>
