17ac5c97ab9c2789a9a3db7afcbdfb290d598404
DFaustino6 <a61171@ualg.pt>
2019-11-04 22:07:12 +0000

    Update newblog_action.php

--- newblog_action.php
+++ newblog_action.php
@@ -15,9 +15,14 @@ if($db) {
   	$query  = "INSERT INTO microposts(user_id,content,created_at,updated_at) VALUES ('$user_id','$content',NOW(),NOW())";
   	$result= @ mysql_query($query,$db);
   	$smarty->assign('Msg', "SUCCESS: New post submitted");	
+  	$smarty->assign('text_color',"green");
+   	$smarty->assign('back_color',"#00d269");
   }
-  else
+  else{
   	$smarty->assign('Msg', "ERROR: Login first");
+  	$smarty->assign('text_color',"red");
+   	$smarty->assign('back_color',"#ff9d9d");
+  }
   mysql_close($db);
 
   $smarty->display('message_template.tpl');
