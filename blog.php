093d1fd0ba973be61294c75ae547c63a2d5b72a3
DFaustino6 <a61171@ualg.pt>
2019-11-04 23:08:40 +0000

    content query

--- blog.php
+++ blog.php
@@ -6,13 +6,22 @@ $smarty = new Smarty();
 $smarty->template_dir = 'templates';
 $smarty->compile_dir = 'templates_c';
 
+$PostId=$_GET['Post_id'];
 
+$db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
+if($db){
+   $query  = "SELECT content * FROM microposts WHERE id='$PostId'";
+   $result = @ mysql_query($query,$db);
+}
+mysql_close($db);
+  
   $smarty->assign('FORUMName',"DAW Lab");
   $smarty->assign('href0',"index.php");
   $smarty->assign('MENU1',"Blog");
   $smarty->assign('href1',"blog.php");
   $smarty->assign('ActionPost',"Write");
-  $PostId=$_GET['Post_id'];
+  $smarty->assign('Post_content',$result);
+ 
   if(isset($_GET['Post_id']))
     $smarty->assign('Action',"updateblog_action.php?Post_id=$PostId");
   else
